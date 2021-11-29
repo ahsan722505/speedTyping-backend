7821374dfbac8724872e9f0730ebac674d2be5a0
ahsan722505 <ahsantahir722505@gmail.com>
2021-11-29 21:26:03 +0500

    djgbbg

--- server.js
+++ server.js
@@ -12,18 +12,18 @@ const io = new Server(httpServer, { cors: {
   let rooms={};
   let validRoomIds=[];
   let times={};
-  const startCountDown=(id)=>{
-    setInterval(()=>{
-      times[id]--;
-      io.to(id).emit("update-time",times[id]);
-     },1000);
-  }
+  // const startCountDown=(id)=>{
+  //   setInterval(()=>{
+  //     times[id]--;
+  //     io.to(id).emit("update-time",times[id]);
+  //    },1000);
+  // }
   io.on("connection",(socket)=>{
       socket.on("create-room",(name)=>{
         const newId = uuidv4();
           socket.join(newId);
           rooms[newId]=[{name : name , id : socket.id}];
-          times[newId]=15;
+          // times[newId]=15;
           validRoomIds.push(newId);
           socket.emit("take-id",newId);
           socket.emit("take-players",rooms[newId])
@@ -33,7 +33,7 @@ const io = new Server(httpServer, { cors: {
         if(validRoomIds.includes(data.roomId)){
           io.to(data.roomId).emit("update-players",{name: data.name,id : socket.id});
           rooms[data.roomId].push({name : data.name , id : socket.id});
-          if(rooms[data.roomId].length === 2) startCountDown(data.roomId);
+          if(rooms[data.roomId].length === 2) times[data.roomId]=new Date(); 
           socket.join(data.roomId);
           socket.emit("proceed",true);
         }else{
@@ -43,6 +43,8 @@ const io = new Server(httpServer, { cors: {
 
       socket.on("give-players",(roomId)=>{
         socket.emit("take-players",rooms[roomId]);
+        io.to(roomId).emit("start-timer",times[roomId]);
+
       })
   })
   httpServer.listen(process.env.PORT || 8080);
